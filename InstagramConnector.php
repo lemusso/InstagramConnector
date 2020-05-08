4738f226f2532b61f06d8b28f2eb59823c9d1bdc
Gabriel Alvarez <gabriel.tandil@gmail.com>
2020-05-08 15:14:24 -0300

    autenticacion de dos pasos

--- InstagramConnector.php
+++ InstagramConnector.php
@@ -35,7 +35,18 @@ try {
     
     $ig = new \InstagramAPI\Instagram($debug, $truncatedDebug);
     
-    $ig->login($username, $password);
+    $loginResponse=$ig->login($username, $password);
+    
+    if ($loginResponse !== null && $loginResponse->isTwoFactorRequired()) {
+        $twoFactorIdentifier = $loginResponse->getTwoFactorInfo()->getTwoFactorIdentifier();
+        
+        // The "STDIN" lets you paste the code via terminal for testing.
+        // You should replace this line with the logic you want.
+        // The verification code will be sent by Instagram via SMS.
+        $verificationCode = trim(fgets(STDIN));
+        $ig->finishTwoFactorLogin($username, $password, $twoFactorIdentifier, $verificationCode);
+    }
+    
 } catch (\Exception $e) {
     echo 'Something went wrong: '.$e->getMessage()."\n";
     exit(0);
